924c2514fcad1b4a1ef0697f21a74448c810cd48
Szeles Laszlo <laszlo.szeles@nitrowise.com>
2025-11-23 10:49:19 +0100

    update to zoneless

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,4 +1,8 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import {
+  ApplicationConfig,
+  provideZoneChangeDetection,
+  provideZonelessChangeDetection,
+} from '@angular/core';
 import {
   provideRouter,
   TitleStrategy,
@@ -7,14 +11,13 @@ import {
 
 import { provideImgixLoader } from '@angular/common';
 import { provideHttpClient } from '@angular/common/http';
-import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { routes } from './app.routes';
 import { TemplatePageTitleStrategy } from './services/title-strategy.service';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
-    provideAnimationsAsync(),
+    provideZonelessChangeDetection(),
     provideHttpClient(),
     provideImgixLoader('http://localhost:4200/'),
     provideRouter(routes, withComponentInputBinding()),
