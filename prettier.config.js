0b44497f3c1f5c7b9ff42e365682877d63385ac1
Szeles Laszlo <laszlo.szeles@nitrowise.com>
2025-12-29 09:03:55 +0100

    add prettier config

---
+++ prettier.config.js
@@ -0,0 +1,16 @@
+// Prettier configuration aligned with the ESLint setup
+// https://prettier.io/docs/en/configuration.html
+module.exports = {
+  printWidth: 100,
+  tabWidth: 2,
+  useTabs: false,
+  semi: true,
+  singleQuote: true,
+  quoteProps: "as-needed",
+  trailingComma: "all",
+  bracketSpacing: true,
+  bracketSameLine: false,
+  arrowParens: "always",
+  proseWrap: "preserve",
+  endOfLine: "lf",
+};
